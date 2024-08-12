58ead11e53e8aae61f68059ca073275bcd038e5e
Michael Kerr <kerrjaxm@gmail.com>
2024-08-12 12:40:41 -0400

    added multiplication table

--- main.py
+++ main.py
@@ -2,7 +2,7 @@
 def get_int():
     while True:
         try:
-            user_num = int(input("Please enter a positive integer: \n"))
+            user_num = int(input("Please enter a positive integer for the range you would like to see: \n"))
             if user_num > 0:
                 return user_num
             else:
@@ -34,7 +34,35 @@ while True:
         print(f"{i}", " "*(8-len(str(i))),
               f"{i**2}", " "*(10-len(str(i**2))),
               f"{i**3}")
-    
+
+    # multiplication table header
+    print("This is a multiplication table for the range you chose!")
+    print("\t", end="")
+    for i in range(1, user_int + 1):
+        print(f"{i}", " "*(3-len(str(i))), end="")
+        if i == user_int:
+            print()
+    print("\t", end="")
+    for i in range(1, user_int + 1):
+        print("=", " "*(3-len(str(i))), end="")
+        if i == user_int:
+            print()
+
+    # setting up format for each row
+    multiply = 1
+    while multiply != user_int + 1:
+
+        print(f"{multiply} | ", end="")
+
+        for i in range(1, user_int + 1):
+            print(f"{i*multiply}", " "*(3-len(str(i*multiply))),
+                  end="")
+
+            if i == user_int:
+                print()
+        multiply += 1
+
+    print()
     # calls a validation function to make sure user only responds yes or no
     user_break = loop_break()
     if user_break == 'n':
